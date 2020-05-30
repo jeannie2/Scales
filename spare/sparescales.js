2cacca1c97e63ff0cad324e4bd7a451641597489
jeannie2 <j.nam@hotmail.com>
2020-05-30 17:22:54 +0800

    Add files via upload

---
+++ spare/sparescales.js
@@ -0,0 +1,13 @@
+
+/*
+div.mouseover(function() {
+console.log("soup");
+div.style.rotate()
+});*/
+ 
+//scale.addEventListener("mouseover", rocket())
+
+ 
+function otherScales(i){
+    /*scale[i].classList.remove('scale'); /* mvoe border css to other place. or just modify scale class */
+  }
